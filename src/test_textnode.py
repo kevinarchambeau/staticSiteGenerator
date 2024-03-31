d3c42b685650fc222e8d558b526f221a8cb70b35
Kevin Archambeau <xxerexx@gmail.com>
2024-03-31 08:05:53 -0700

    couple more unit checks

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,15 +4,25 @@ from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
+    def test_string_eq(self):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
-    def test_not_eq(self):
+    def test_string_not_eq(self):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold", "other")
         self.assertNotEqual(node, node2)
+
+    def test_eq_is_true(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertTrue(node.__eq__(node2))
+
+    def test_eq_is_false(self):
+        node = TextNode("This is a text node", "something", "other")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertFalse(node.__eq__(node2))
 
 if __name__ == "__main__":
     unittest.main()
