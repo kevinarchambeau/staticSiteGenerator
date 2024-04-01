642b1cb02e0a64c656ed1fe7f0dfcd117c621fe7
Kevin Archambeau <xxerexx@gmail.com>
2024-04-01 16:30:23 -0700

    finished implementing to_html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,5 +37,8 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return self.value
 
-        return f'"{self.tag}"'
+        if self.props:
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+        return f"<{self.tag}>{self.value}</{self.tag}>"
 
