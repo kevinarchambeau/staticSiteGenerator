5d93e8ce196d46d957363d141c3b14ab56d7e1fd
Kevin Archambeau <xxerexx@gmail.com>
2024-04-03 15:09:31 -0700

    fixed a bug in props_to_html, implemented ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,18 +14,17 @@ class HTMLNode:
     def props_to_html(self):
         html_list = []
 
+        if not self.props:
+            return ""
+
         for k, v in self.props.items():
-            html_list.append(f'{k}="{v}"')
-        return " ".join(html_list)
+            html_list.append(f' {k}="{v}"')
+        return "".join(html_list)
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag = None, props = None):
+    def __init__(self, tag, value, props = None):
         super().__init__(tag, value, None, props)
-
-        self.value = value
-        self.tag = tag
-        self.props = props
 
     def __repr__(self):
         return f"LeafNode(tag: {self.tag}, value: {self.value}, props: {self.props})"
@@ -37,8 +36,26 @@ class LeafNode(HTMLNode):
         if not self.tag:
             return self.value
 
-        if self.props:
-            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-        return f"<{self.tag}>{self.value}</{self.tag}>"
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, value = None, children = children, props = props)
 
+    def __repr__(self):
+        return f"ParentNode(children: {self.children}, tag: {self.tag}, props: {self.props})"
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError
+
+        if not self.children:
+            raise ValueError("No children")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+
+        for child in self.children:
+            html += child.to_html()
+
+        html += f"</{self.tag}>"
+        return html
