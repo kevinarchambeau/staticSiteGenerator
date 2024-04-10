d990f78c1b76372a2eb9e5a78235afff854d7cb2
Kevin Archambeau <xxerexx@gmail.com>
2024-04-10 16:43:11 -0700

    added text_node_to_html_node method

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,8 @@
+from htmlnode import LeafNode
+
+
 class TextNode:
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -14,3 +17,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError("Not a valid text type")
