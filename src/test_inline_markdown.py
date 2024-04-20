bbb15fde4c02cad4d242f6e3bb0890f436f849b7
Kevin Archambeau <xxerexx@gmail.com>
2024-04-20 10:18:12 -0700

    add text to textnode method

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -3,7 +3,8 @@ from textnode import (
     TextNode, text_type_text, text_type_bold, text_type_italic, text_type_code, text_type_image, text_type_link
 )
 from inline_markdown import (
-    split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+    split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link,
+    text_to_textnodes
 )
 
 
@@ -83,6 +84,24 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_text_to_textnode(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(nodes, [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("image", text_type_image,
+                     "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+                         )
 
 
 if __name__ == "__main__":
