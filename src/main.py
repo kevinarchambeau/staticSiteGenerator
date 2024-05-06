b63cbc8bdc4a0d0fbbc627c5de0be39df3e80850
Kevin Archambeau <xxerexx@gmail.com>
2024-05-06 15:48:36 -0700

    added page generation, updated main.sh

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-from textnode import TextNode
+from inline_markdown import extract_title
+from block_markdown import markdown_to_html_node
 
 
 def setup_public():
@@ -10,10 +11,27 @@ def setup_public():
     shutil.copytree("static", "public")
 
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"generating {from_path}, using {template_path}, to {dest_path}")
+    markdown = open(from_path).read()
+    destination = open(dest_path, "w")
+    title = extract_title(markdown)
+    if not title:
+        raise ValueError("No title found")
+    stripped_title = title.group(0).lstrip("#").strip()
+    template = open(template_path).read()
+    node = markdown_to_html_node(markdown)
+    html_to_insert = node.to_html()
+    template_with_title = template.replace("{{ Title }}", stripped_title, 1)
+    page = template_with_title.replace("{{ Content }}", html_to_insert, 1)
+    destination.write(page)
+    destination.close()
+
+
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
     setup_public()
+    print("converting file")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
