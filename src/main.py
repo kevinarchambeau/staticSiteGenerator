c70a3a094b0aa0f091d554221197c23ff099db43
Kevin Archambeau <xxerexx@gmail.com>
2024-05-06 12:28:37 -0700

    mistake, added setup method

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+import os
+import shutil
+
 from textnode import TextNode
+
+
+def setup_public():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    shutil.copytree("static", "public")
 
 
 def main():
     node = TextNode("This is a text node", "bold", "https://www.boot.dev")
     print(node)
+    setup_public()
 
 
 main()
